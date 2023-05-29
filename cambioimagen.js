8fcd818416aba7229f8b1902b6e97de41e14ee9e
yunru070419 <134432357+yunru070419@users.noreply.github.com>
2023-05-29 07:47:38 -0400

    Add files via upload

---
+++ cambioimagen.js
@@ -0,0 +1,13 @@
+function img1(){
+    document.getElementById("imagen").src="cambiodeimagen1.png";
+}
+function img2(){
+    document.getElementById("imagen").src="cambiodeimagen2.png";
+}
+function img3(){
+    document.getElementById("imagen").src="cambiodeimagen3.png";
+}
+function img4(){
+    document.getElementById("imagen").src="cambiodeimagen4.png";
+}
+
